8583c27f1202e920191f185b482acff5f6d2a4bb
decoch <tominaga.switch@gmail.com>
2021-11-13 15:08:02 +0900

    feat: print config log

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,30 @@
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
 void main(List<String> args) {
-  print(args);
+  final currentPath = Directory.current.path;
+
+  final pubspecYamlFile = File('${currentPath}/pubspec.yaml');
+  final pubspecYaml = loadYaml(pubspecYamlFile.readAsStringSync());
+
+  final packageName = pubspecYaml['name'];
+  final dependencies = [];
+
+  final stopwatch = Stopwatch();
+  stopwatch.start();
+
+  final pubspecLockFile = File('${currentPath}/pubspec.lock');
+  final pubspecLock = loadYaml(pubspecLockFile.readAsStringSync());
+  dependencies.addAll(pubspecLock['packages'].keys);
+
+  final configYamlFile = File('${currentPath}/strict_dependencies.yaml');
+  final configYaml = loadYaml(configYamlFile.readAsStringSync());
+
+  // Reading from config in pubspec.yaml safely
+  if (configYaml.containsKey('rules')) {
+    final config = configYaml['rules'];
+    print(packageName);
+    print(config);
+  }
 }
